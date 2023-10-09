6de58efc1af0285bc2c95eb8947123e519a47da2
Afiifatuts Tsaaniyah Abdullah <afiifatuts04@gmail.com>
2023-10-09 11:28:09 +0700

    🎨 creating view layout

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Main::index');
+$routes->get('/kategori/index', 'Kategori::index');
+$routes->get('/satuan/index', 'Satuan::index');
+$routes->get('/barang/index', 'Barang::index');
---
+++ app/Views/kategori/viewkategori.php
@@ -0,0 +1,13 @@
+<?= $this->extend('main/layout') ?>
+
+<?= $this->section('judul') ?>
+Manajemen Data Kategori
+<?= $this->endSection('judul') ?>
+
+<?= $this->section('subjudul') ?>
+Data Kategori
+<?= $this->endSection('subjudul') ?>
+
+<?= $this->section('isi') ?>
+ini adalah menu kategori
+<?= $this->endSection('isi') ?>
